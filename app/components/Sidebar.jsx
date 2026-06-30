fdad53a71f076f6ec7ae1ca536c441cf9910bf79
mycarrieracademic-sketch <mycarrieracademic@gmail.com>
2026-06-30 12:04:56 +0530

    Update Sidebar.jsx

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -25,8 +25,14 @@ export default function Sidebar({ role, activeTab, setActiveTab, onLogout }) {
       display: "flex", flexDirection: "column",
       position: "fixed", left: 0, top: 0, zIndex: 100
     }}>
-      <div style={{ padding: "16px", borderBottom: "1px solid #ffffff20", display: "flex", justifyContent: "center" }}>
-        <Image src="/mca-logo.png" alt="My Career Academic" width={160} height={70} style={{ objectFit: "contain", width: "100%", height: "auto" }} priority />
+      <div style={{ padding: "14px 16px", borderBottom: "1px solid #ffffff20", display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <Image src="/mca-logo.png" alt="My Career Academic" width={90} height={90} style={{ objectFit: "contain" }} priority />
+        <div style={{ fontSize: "12px", fontWeight: "700", color: "white", marginTop: "8px", textAlign: "center", letterSpacing: "0.3px" }}>
+          MY CAREER ACADEMIC
+        </div>
+        <div style={{ fontSize: "9px", color: "#ffffff80", marginTop: "2px", textAlign: "center", letterSpacing: "0.3px" }}>
+          DIVISION OF MLF GROUP
+        </div>
       </div>
       <div style={{ flex: 1, overflowY: "auto", padding: "8px 0" }}>
         {tabs.map(tab => (
